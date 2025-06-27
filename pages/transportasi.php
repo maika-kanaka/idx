51adc8b87a7493ba46fb874a99a1d902be9ff5e2
Budi Irwan Firmansyah <maika.kanaka@gmail.com>
2025-06-27 22:06:16 +0700

    tambah lokasi wisata dan page transportasi

--- pages/transportasi.php
+++ pages/transportasi.php
@@ -1,6 +1,79 @@
-<div class="uk-container">
-    <?php include 'components/logo.php'; ?>
-    <h1 class="uk-heading-large uk-text-center">Transportasi</h1>
-    
-    
+<div class="transportasi-page">
+    <!-- Flight Booking Card -->
+    <div class="flight-booking-container">
+        <div class="flight-card">
+            <!-- Flight Route Header -->
+            <div class="flight-route">
+                <div class="airport-code">CGK</div>
+                <div class="flight-icon">
+                    <svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M21 16v-2l-8-5V3.5c0-.83-.67-1.5-1.5-1.5S10 2.67 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5l8 2.5z" fill="black"/>
+                        <line x1="3" y1="12" x2="21" y2="12" stroke="black" stroke-width="1" stroke-dasharray="5,5"/>
+                    </svg>
+                </div>
+                <div class="airport-code">BTH</div>
+            </div>
+
+            <!-- Flight Details Form -->
+            <div class="flight-details">
+                <!-- Airplane Image -->
+                <div class="airplane-image">
+                    <div class="airplane-circle">
+                    <svg width="40" height="40" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M21 16v-2l-8-5V3.5c0-.83-.67-1.5-1.5-1.5S10 2.67 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5l8 2.5z" fill="black"/>
+                        <line x1="3" y1="12" x2="21" y2="12" stroke="black" stroke-width="1" stroke-dasharray="5,5"/>
+                    </svg>
+                    </div>
+                </div>
+
+                <!-- Form Fields -->
+                <div class="form-section">
+                    <div class="form-group">
+                        <label>Dari</label>
+                        <input type="text" value="Jakarta" readonly>
+                    </div>
+                    
+                    <div class="form-group">
+                        <label>Ke</label>
+                        <input type="text" value="Batam" readonly>
+                    </div>
+                    
+                    <div class="form-row">
+                        <div class="form-group">
+                            <label>Keberangkatan</label>
+                            <input type="text" value="11 Juli 2025" readonly>
+                        </div>
+                        <div class="form-group">
+                            <label>Kedatangan</label>
+                            <input type="text" value="11 Juli 2025" readonly>
+                        </div>
+                    </div>
+                    
+                    <div class="form-row">
+                        <div class="form-group">
+                            <label>Waktu Keberangkatan</label>
+                            <input type="text" value="09:00 WIB" readonly>
+                        </div>
+                        <div class="form-group">
+                            <label>Waktu Kedatangan</label>
+                            <input type="text" value="11:00 WIB" readonly>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Contact Person -->
+            <div class="contact-person">
+                <h3>Ny. Roos Eliza Putri</h3>
+                <div class="contact-info">
+                    <p>1. Peserta diharapkan datang <strong>30 menit sebelum waktu keberangkatan</strong></p>
+                    <p>2. Jika membawa bagasi harap di informasikan kepada Tour Leader</p>
+                    <p>3. Dan Lain-Lain</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- City Skyline Background -->
+    <div class="city-skyline"></div>
 </div> 
--- pages/wisata.php
+++ pages/wisata.php
@@ -1,38 +1,50 @@
 <div class="uk-container">
+    <?php include 'components/logo.php'; ?>
+
     <h1 class="uk-heading-large uk-text-center">Informasi Wisata</h1>
-    <div class="uk-grid-match uk-child-width-1-3@m" uk-grid>
-        <div>
-            <div class="uk-card uk-card-default uk-card-body">
-                <h3>Batam</h3>
-                <ul class="uk-list uk-list-bullet">
-                    <li>Barelang Bridge</li>
-                    <li>Maha Vihara Duta Maitreya</li>
-                    <li>Pantai Nongsa</li>
-                    <li>Batam Centre</li>
-                </ul>
+
+    <div id="info-wisata-slideshow" class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="autoplay: true; autoplay-interval: 2000; infinite: true">
+
+        <div class="uk-slideshow-items">
+            <div>
+                <img src="https://portal.migunesia.com/storage/anu-anu-builder/event-2352/2352-gallery-685ae8d55b9d9.png" alt="" uk-cover>
+                <div class="uk-overlay uk-overlay-default uk-position-bottom-right uk-position-small">
+                    <h3 class="uk-margin-remove">Masjid Tanjak</h3>
+                    <p class="uk-margin-remove">Masjid Tanjak Batam, atau nama resminya Masjid Tanwirun Naja, adalah sebuah tempat ibadah sekaligus ikon wisata religi yang menonjol di Kota Batam, Kepulauan Riau. Masjid ini sangat dikenal karena arsitekturnya yang unik, menyerupai tanjak Melayu, yaitu penutup kepala khas masyarakat Melayu yang runcing ke atas.</p>
+                </div>
+            </div>
+            <div>
+                <img src="https://portal.migunesia.com/storage/anu-anu-builder/event-2352/2352-gallery-685aede3bb7a2.png" alt="" uk-cover>
+                <div class="uk-overlay uk-overlay-default uk-position-bottom-right uk-position-small">
+                    <h3 class="uk-margin-remove">Grand Mall Batam</h3>
+                    <p class="uk-margin-remove">Grand Batam Mall adalah sebuah pusat perbelanjaan dinamis yang berlokasi di Jl. Pembangunan, Batu Selicin, Kec. Lubuk Baja, Kota Batam, Kepulauan Riau 29444. Mal ini menawarkan berbagai fasilitas, termasuk department store, butik, tempat makan kasual, dan arkade anak-anak.</p>
+                </div>
+            </div>
+            <div>
+                <img src="https://portal.migunesia.com/storage/anu-anu-builder/event-2352/2352-gallery-685aede90641e.jpg" alt="" uk-cover>
+                <div class="uk-overlay uk-overlay-default uk-position-bottom-right uk-position-small">
+                    <h3 class="uk-margin-remove">Jembatan Barelang</h3>
+                    <p class="uk-margin-remove">Jembatan Barelang adalah sebuah rangkaian jembatan ikonik yang menghubungkan beberapa pulau di Provinsi Kepulauan Riau, Indonesia. Secara khusus, jembatan ini menghubungkan pulau-pulau utama Batam, Rempang, dan Galang, serta pulau-pulau kecil di antaranya.</p>
+                </div>
+            </div>
+            <div>
+                <img src="https://guestbook.migunesia.com/storage/anu-anu-builder/event-2352/harbour.jpg" alt="" uk-cover>
+                <div class="uk-overlay uk-overlay-default uk-position-bottom-right uk-position-small">
+                    <h3 class="uk-margin-remove">BATAM HARBOUR BAY</h3>
+                    <p class="uk-margin-remove">Batam Harbour Bay adalah sebuah area di Batam yang dikenal sebagai kawasan pelabuhan feri dan pusat kegiatan maritim.</p>
+                </div>
+            </div>
+            <div>
+                <img src="https://guestbook.migunesia.com/storage/anu-anu-builder/event-2352/batamia.jpg" alt="" uk-cover>
+                <div class="uk-overlay uk-overlay-default uk-position-bottom-right uk-position-small">
+                    <h3 class="uk-margin-remove">Batamia</h3>
+                    <p class="uk-margin-remove">Wisata Oleh Oleh Batamia adalah sebuah toko yang menjual oleh-oleh khas Batam. Toko ini berlokasi di Jl. Nagoya Thamrin, Lubuk Baja Kota, Kec. Lubuk Baja, Kota Batam, Kepulauan Riau 29444.</p>
+                </div>
             </div>
         </div>
-        <div>
-            <div class="uk-card uk-card-default uk-card-body">
-                <h3>Singapore</h3>
-                <ul class="uk-list uk-list-bullet">
-                    <li>Marina Bay Sands</li>
-                    <li>Gardens by the Bay</li>
-                    <li>Chinatown</li>
-                    <li>Orchard Road</li>
-                </ul>
-            </div>
-        </div>
-        <div>
-            <div class="uk-card uk-card-default uk-card-body">
-                <h3>Johor Baru</h3>
-                <ul class="uk-list uk-list-bullet">
-                    <li>Legoland Malaysia</li>
-                    <li>Johor Premium Outlets</li>
-                    <li>Sultan Abu Bakar Mosque</li>
-                    <li>Danga Bay</li>
-                </ul>
-            </div>
-        </div>
+
+        <a class="uk-position-center-left uk-position-small uk-hidden-hover" href uk-slidenav-previous uk-slideshow-item="previous"></a>
+        <a class="uk-position-center-right uk-position-small uk-hidden-hover" href uk-slidenav-next uk-slideshow-item="next"></a>
+
     </div>
-</div> 
+</div>
